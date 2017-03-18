edf4130c3d613b1dd5dfd019e53028e6380f2f50
Tim Freeman <d.timothy.freeman@gmail.com>
2017-03-18 14:15:10 -0400

    The Bachelor To Do Prework -- Passed Tests

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,86 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+	winner_first_name = ""
+	data.each do |season_number, contestant_array|
+		if season_number == season
+			contestant_array.each do |element|
+				element.each do |key, value|
+					if element.values.include?("Winner")
+						if key == "name"
+							winner_first_name = value.split(" ")[0]
+						end
+					end
+				end
+			end
+		end
+	end
+	winner_first_name
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+	contestant_name = ""
+	data.each do |season_number, contestant_array|
+		contestant_array.each do |element|
+			element.each do |key, value|
+				if element.values.include?(occupation)
+					if key == "name"
+						contestant_name = value
+					end
+				end
+			end
+		end
+	end
+	contestant_name
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+	count = 0
+	data.each do |season_number, contestant_array|
+		contestant_array.each do |element|
+			element.each do |key, value|
+				if key == "hometown" && value == hometown
+					count +=1
+				end
+			end
+		end
+	end
+	count
 end
 
 def get_occupation(data, hometown)
-  # code here
+	occupation = ""
+	count = 0
+	data.each do |season_number, contestant_array|
+		contestant_array.each do |element|
+			element.find do |key, value|
+				if element.values.include?(hometown) && count < 1
+					if key == "occupation"
+						occupation = value
+						count +=1
+					end
+				end
+			end
+		end
+	end
+	occupation
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+	average_age = 0
+	ages_array =[]
+	data.each do |season_number, contestant_array|
+		if season_number == season
+			contestant_array.each do |element|
+				element.each do |key, value|
+					if key == "age"
+						ages_array << value.to_f
+					end
+				end
+			end
+		end
+	end
+	ages_array.each do |age|
+		average_age += age
+	end
+	average_age = (average_age/ages_array.size).round
+	average_age
 end
